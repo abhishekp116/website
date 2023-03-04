4d380a3604a84ec0e29faeb9d6544c5288872625
abhishekp116 <126357110+abhishekp116@users.noreply.github.com>
2023-03-04 09:50:42 +0530

    Initial commit

---
+++ _next/static/XsvQzLRYtb2Bk_G1MMk6p/_buildManifest.js
@@ -0,0 +1,45 @@
+(self.__BUILD_MANIFEST = (function (s, a, c, e) {
+  return {
+    __rewrites: { beforeFiles: [], afterFiles: [], fallback: [] },
+    "/": [a, s, c, e, "static/chunks/pages/index-a300e6260d98608b.js"],
+    "/404": [s, "static/chunks/pages/404-b6e6e242c104d231.js"],
+    "/500": [s, "static/chunks/pages/500-ef7492c9606adceb.js"],
+    "/_error": ["static/chunks/pages/_error-a4ba2246ff8fb532.js"],
+    "/article/[articleSlug]": [
+      "static/chunks/pages/article/[articleSlug]-f9a363e77f8ff468.js",
+    ],
+    "/case/[caseSlug]": [
+      a,
+      s,
+      "static/chunks/pages/case/[caseSlug]-8c4c1c105f611562.js",
+    ],
+    "/cases": [a, s, c, e, "static/chunks/pages/cases-856bfb448c5ceaf7.js"],
+    "/contact": [a, s, c, e, "static/chunks/pages/contact-c8d77d843f115d50.js"],
+    "/sitemap.xml": ["static/chunks/pages/sitemap.xml-05cf86110135ff08.js"],
+    "/[pageSlug]": [
+      a,
+      s,
+      c,
+      "static/chunks/pages/[pageSlug]-f72551acb47de818.js",
+    ],
+    sortedPages: [
+      "/",
+      "/404",
+      "/500",
+      "/_app",
+      "/_error",
+      "/article/[articleSlug]",
+      "/case/[caseSlug]",
+      "/cases",
+      "/contact",
+      "/sitemap.xml",
+      "/[pageSlug]",
+    ],
+  };
+})(
+  "static/chunks/371-fad00b98516d7a60.js",
+  "static/chunks/423-04798f16f2c16203.js",
+  "static/chunks/866-941a483fa9fd2656.js",
+  "static/chunks/176-557fe0bb7410b795.js"
+)),
+  self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB();
---
+++ _next/static/XsvQzLRYtb2Bk_G1MMk6p/_ssgManifest.js
@@ -0,0 +1,8 @@
+self.__SSG_MANIFEST = new Set([
+  "\u002F",
+  "\u002F[pageSlug]",
+  "\u002Farticle\u002F[articleSlug]",
+  "\u002Fcase\u002F[caseSlug]",
+  "\u002Fcontact",
+]);
+self.__SSG_MANIFEST_CB && self.__SSG_MANIFEST_CB();
